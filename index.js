df33f3c987d9d33853380d7c68cff98314b6531a
aram <almustafa.ahmadrami@gmail.com>
2026-02-05 22:27:38 +0100

    added static middleware

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 
 app.use((request, response, next) => {
@@ -13,7 +12,7 @@ app.use(express.json())
 morgan.token('body', (request, response) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
   { 
